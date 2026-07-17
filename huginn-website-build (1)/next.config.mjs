2a449c4d53fca2cf12d234c27a568524b299903f
Biafgi <angelofmight12@gmail.com>
2026-07-17 11:29:09 +0100

    infra: Make static export conditional to support both Express hosting and standalone dynamic Next.js service

--- huginn-website-build (1)/next.config.mjs
+++ huginn-website-build (1)/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  ...(process.env.STATIC_EXPORT === 'true' ? { output: 'export' } : {}),
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
